ca13959372c0c9a01e4182b6f5cdfd0b09f003f2
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-08-09 01:25:33 +0500

    update

---
+++ Exercise19/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/* Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.*/
+// Importing Guests List from exercise 15
+const Exercise15_1 = require("../Exercise15");
+console.log(`\nPrinting message indicating number of guests`);
+console.log(`We had finally invited ${Exercise15_1.guestList.length} guest in Exercise 15.`);
