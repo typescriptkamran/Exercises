c8881f58c1d8869239a8126e050594fcc532e335
Muhammad Kamran <139162111+typescriptkamran@users.noreply.github.com>
2023-08-12 00:31:01 +0500

    Update index.ts

--- Exercise3/index.ts
+++ Exercise3/index.ts
@@ -17,25 +17,11 @@ console.log(uppercaseName);
 // step 3 show name in titlecase
 
 let words: string[] = PersonName.split(" ");
-console.log(words)
-console.log(words[0])
-let titlecase = words[0].charAt(0).toUpperCase()
-console.log(titlecase)
 
-// let titlecaseName: string = ""
+let titlecaseName: string = ""
 
-// for (let i = 0 ; i < words.length; i++) {
-//     titlecaseName +=words[i].charAt(0).toUpperCase() +words[i].slice(1).toLowerCase()+" "
-// };
+for (let i = 0 ; i < words.length; i++) {
+    titlecaseName +=words[i].charAt(0).toUpperCase() +words[i].slice(1).toLowerCase()+" "
+};
 
-
-
-
-// console.log(titlecaseName)
-
-// console.log(3 + +"3")
-
-// comparison operatiors 
-
-
-
+console.log(titlecaseName)
